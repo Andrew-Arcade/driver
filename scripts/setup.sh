e5d7c54df4f63fc7a08ecd93bed5e4f575991e98
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-03-17 10:12:48 -0700

    temp: testing

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,20 @@
-echo "Hello World!"
+#!/bin/bash
+
+echo "---------- Setting Up Andrew Arcade ----------"
+
+USER="arcade"
+
+if id "$USER" &>/dev/null; then
+    echo "User '$USER' already exists."
+else
+    echo "User '$USER' does not exist. Creating..."
+    
+    sudo useradd -m "$USER"
+    
+    if [ $? -eq 0 ]; then
+        echo "User '$USER' created successfully."
+    else
+        echo "Failed to create user '$USER'."
+        exit 1
+    fi
+fi
