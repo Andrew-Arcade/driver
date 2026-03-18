a9d4da03781cbbeb9f7d741f933db2ee186a26aa
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-03-18 12:09:47 -0700

    update version label

--- godot-project/core/scripts/version_label.gd
+++ godot-project/core/scripts/version_label.gd
@@ -1,4 +1,4 @@
 extends Label
 
 func _ready() -> void:
-	text = "v" + ProjectSettings.get_setting("application/config/version")
+	text = "driver-v" + ProjectSettings.get_setting("application/config/version")
