5b06a2240894345859a4cf81cc013a6cace0ccf1
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-03-18 10:51:33 -0700

    feat: add update button

---
+++ godot-project/scenes/host_manager/host_manager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+func _shutdown() -> void:
+	print("Shutting down system...")
+	
+	var output = []
+	var exit_code = OS.execute("/usr/bin/sudo", ["/usr/bin/systemctl", "poweroff"], output)
+	
+	if exit_code == -1: print("Failed to execute shutdown command.")
+
+func _reboot() -> void:
+	print("Rebooting system...")
+
+	var output = []
+	var exit_code = OS.execute("/usr/bin/sudo", ["/usr/bin/systemctl", "reboot"], output)
+
+	if exit_code == -1: print("Failed to execute reboot command.")
+
+func _update() -> void:
+	print("Updating driver...")
+	OS.create_process("/usr/bin/setsid", ["/bin/bash", "/andrewarcade/driver/scripts/launch.sh"])
+	get_tree().quit()
