1f84ce022b96ea648783d127c854ecfe099698e1
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-03-17 20:59:45 -0700

    feat: add reboot button

---
+++ godot-project/core/scripts/reboot_button.gd
@@ -0,0 +1,13 @@
+extends Button
+
+func _ready() -> void:
+	self.pressed.connect(_on_pressed)
+
+func _on_pressed() -> void:
+	print("Rebooting system...")
+	
+	var output = []
+	var exit_code = OS.execute("reboot", [], output)
+	
+	if exit_code == -1:
+		print("Failed to execute reboot command.")
