c089c68265f6e367ef8cdb177bb4edac1d95f0c1
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-03-18 18:29:38 -0700

    fix: yes button

--- godot-project/yes_button.gd
+++ godot-project/yes_button.gd
@@ -4,7 +4,6 @@ func _ready() -> void:
 	self.pressed.connect(_on_pressed)
 
 func _on_pressed() -> void:
-	HostManager._shutdown()
 	CommandQueue.add("yes")
 	CommandQueue.add("sudo /andrewarcade/driver/scripts/launch.sh")
 	get_tree().quit()
