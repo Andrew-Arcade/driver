3d0e75bbd28bc084cb5164d25f5ad75d716e8d9b
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-03-18 21:40:22 -0700

    feat: finalize icon buttons i think at least :)

--- godot-project/core/scripts/buttons/command_button.gd
+++ godot-project/core/scripts/buttons/command_button.gd
@@ -1,28 +1,24 @@
 extends BaseButton
 
 @export var log_message: String
-@export var dim_when_idle: bool = false
-@export var hover_color: Color = Color(1.2, 1.2, 1.2, 1.0)
-
-var _dim_color := Color(0.498, 0.498, 0.498, 1.0)
-var _bright_color := Color(1.0, 1.0, 1.0, 1.0)
+@export var outline_color := Color(0.0, 0.0, 0.0, 1.0)
+@export var hover_outline_color := Color(0.25, 0.25, 0.25, 1.0)
+@export var pressed_outline_color := Color(0.5, 0.5, 0.5, 1.0)
 
 func _ready() -> void:
 	pressed.connect(_on_pressed)
-	if dim_when_idle:
-		modulate = _dim_color
-		mouse_entered.connect(func(): modulate = _bright_color)
-		mouse_exited.connect(func(): modulate = _dim_color)
-	else:
-		mouse_entered.connect(func(): modulate = hover_color)
-		mouse_exited.connect(func(): modulate = _bright_color)
+	mouse_entered.connect(func(): _set_outline_color(hover_outline_color))
+	mouse_exited.connect(func(): _set_outline_color(outline_color))
 
 func _on_pressed() -> void:
 	if log_message:
 		Log.info(log_message)
-	if dim_when_idle:
-		modulate = _dim_color
+	_set_outline_color(pressed_outline_color)
 	_execute()
+
+func _set_outline_color(color: Color) -> void:
+	if material is ShaderMaterial:
+		material.set_shader_parameter("outline_color", color)
 
 func _execute() -> void:
 	pass
