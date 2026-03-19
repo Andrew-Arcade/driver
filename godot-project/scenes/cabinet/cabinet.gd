5eb9fb1a75f0c96a0c3c52cf7503ad0203970a56
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-03-18 22:28:55 -0700

    feat: add remove button

--- godot-project/scenes/cabinet/cabinet.gd
+++ godot-project/scenes/cabinet/cabinet.gd
@@ -7,11 +7,13 @@ extends Control
 @onready var developer_label : Label = %Developer
 @onready var description_label : Label = %Description
 @onready var install_button : TextureButton = %Install
+@onready var remove_button : TextureButton = %Remove
 
 func _ready():
 	if cabinet_data:
 		load_data(cabinet_data)
 	install_button.pressed.connect(_on_install_pressed)
+	remove_button.pressed.connect(_on_remove_pressed)
 
 func _on_install_pressed():
 	if not cabinet_data or cabinet_data.repo_url == "":
@@ -20,7 +22,15 @@ func _on_install_pressed():
 	var install_path = _get_install_path()
 	Log.info("Installing " + cabinet_data.display_name + " to " + install_path)
 	Shell.command("git clone " + cabinet_data.repo_url + " " + install_path)
-	_update_install_button()
+	_update_buttons()
+
+func _on_remove_pressed():
+	if not cabinet_data:
+		return
+	var install_path = _get_install_path()
+	Log.info("Removing " + cabinet_data.display_name + " from " + install_path)
+	Shell.command("rm -rf " + install_path)
+	_update_buttons()
 
 func load_data(data: CabinetData):
 	if display_name_label:
@@ -37,15 +47,19 @@ func load_data(data: CabinetData):
 			icon_rect.texture = data.icon
 		_apply_stretch(icon_rect, Vector2(128, 128))
 
-	_update_install_button()
+	_update_buttons()
 
 func _get_install_path() -> String:
 	var repo_name = cabinet_data.repo_url.get_file()
 	return "/andrewarcade/cabinets/" + repo_name
 
-func _update_install_button():
-	if install_button and cabinet_data:
-		install_button.visible = not DirAccess.dir_exists_absolute(_get_install_path())
+func _update_buttons():
+	if cabinet_data:
+		var installed = DirAccess.dir_exists_absolute(_get_install_path())
+		if install_button:
+			install_button.visible = not installed
+		if remove_button:
+			remove_button.visible = installed
 
 func _apply_stretch(rect: TextureRect, _size: Vector2):
 	rect.custom_minimum_size = _size
