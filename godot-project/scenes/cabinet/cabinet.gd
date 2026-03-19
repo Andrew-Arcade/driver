5182210e00c649c3555bd9e9533b3f58f8c487de
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-03-18 22:25:24 -0700

    feat: check if installed

--- godot-project/scenes/cabinet/cabinet.gd
+++ godot-project/scenes/cabinet/cabinet.gd
@@ -17,25 +17,35 @@ func _on_install_pressed():
 	if not cabinet_data or cabinet_data.repo_url == "":
 		Log.warn("No repo URL available for install.")
 		return
-	var repo_name = cabinet_data.repo_url.get_file()
-	var install_path = "/andrewarcade/cabinets/" + repo_name
+	var install_path = _get_install_path()
 	Log.info("Installing " + cabinet_data.display_name + " to " + install_path)
 	Shell.command("git clone " + cabinet_data.repo_url + " " + install_path)
+	_update_install_button()
 
 func load_data(data: CabinetData):
 	if display_name_label:
 		display_name_label.text = data.display_name
-	
+
 	if developer_label:
 		developer_label.text = "By: " + data.developer
-		
+
 	if description_label:
 		description_label.text = data.description
-	
+
 	if icon_rect:
 		if data.icon:
 			icon_rect.texture = data.icon
 		_apply_stretch(icon_rect, Vector2(128, 128))
+
+	_update_install_button()
+
+func _get_install_path() -> String:
+	var repo_name = cabinet_data.repo_url.get_file()
+	return "/andrewarcade/cabinets/" + repo_name
+
+func _update_install_button():
+	if install_button and cabinet_data:
+		install_button.visible = not DirAccess.dir_exists_absolute(_get_install_path())
 
 func _apply_stretch(rect: TextureRect, _size: Vector2):
 	rect.custom_minimum_size = _size
